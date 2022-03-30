eb81e82d9d49772ee3b805396d6d36e82b49d4af
Afsar Uddin <afsarwebdev@gmail.com>
2022-03-30 12:33:09 +0600

    product item dynamically add using server.js file

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 import express from "express";
 import dotenv from "dotenv"
+import products from "../shoe-shop-server/data/ProductItem.js"
 
 dotenv.config()
 const app = express();
 
+
+// LOAD PRODUCT FROM SERVER
+app.get("/api/products", (req, res) => {
+    res.json(products)
+});
+
+// LOAD SINGLE PRODUCT FROM SERVER
+app.get("/api/products/:id", (req, res) => {
+    const product = products.find((p => p._id === req.params.id))
+    res.json(product)
+});
 
 app.get("/", (req, res) => {
     res.send("Yes, Server is running...")
